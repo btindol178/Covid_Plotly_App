9d61297ca95118821e94b883051b09a9c0919137
btindol178 <datamaster178@gmail.com>
2021-12-14 22:05:33 -0500

    Add files via upload

---
+++ covidapp_setup.py
@@ -0,0 +1,11 @@
+#Step 1 Make sure you are in virtual env
+pip install virtualenv
+
+#Step 2 run virutal env
+virtualenv venv 
+
+#Step 3 move into correct part of venv 
+.\venv\Scripts\activate
+
+#Step 4 run requirments 
+pip install -r requirments.txt
